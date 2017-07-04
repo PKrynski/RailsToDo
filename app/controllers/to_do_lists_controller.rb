f03975ab22f4770dedd05c62f01974059fe0f0db
Pawel Krynski <krynskip@ee.pw.edu.pl>
2017-07-04 20:39:49 +0200

    Create new lists functionality.

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -4,4 +4,17 @@ class ToDoListsController < ApplicationController
 
   end
 
+  def new
+    @to_do_list = ToDoList.new
+  end
+
+  def create
+    @to_do_list = ToDoList.new(params.require(:to_do_list).permit(:title, :description))
+    if @to_do_list.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
 end
