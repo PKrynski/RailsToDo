92566e9a59ac76dbf80e646fa34b95bb8064db6b
Pawel Krynski <krynskip@ee.pw.edu.pl>
2017-07-04 21:06:19 +0200

    Added functionality to show lists, edit and delete them.

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -1,7 +1,12 @@
 class ToDoListsController < ApplicationController
 
-  def index
+  before_action :find_list, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @all_lists = ToDoList.all
+  end
+
+  def show
   end
 
   def new
@@ -9,12 +14,38 @@ class ToDoListsController < ApplicationController
   end
 
   def create
-    @to_do_list = ToDoList.new(params.require(:to_do_list).permit(:title, :description))
+    @to_do_list = ToDoList.new(to_do_list_params)
     if @to_do_list.save
       redirect_to root_path
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @to_do_list.update(to_do_list_params)
+      redirect_to to_do_list_path(@to_do_list)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @to_do_list.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def to_do_list_params
+    params.require(:to_do_list).permit(:title, :description)
+  end
+
+  def find_list
+    @to_do_list = ToDoList.find(params[:id])
   end
 
 end
